0ee18ceca49c9e53820b74e40bedd7579878a6eb
Gulmurod Yuldoshov <gystiu1995@gmail.com>
2022-04-29 01:32:23 +0500

    man

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@ var a = (500+250)*11191
 var b = 120*11747.42
 var summ = a+b
 
+
+
 alert ("Xush kelibsiz");
  var sum=prompt("summani kirgizing", );
 
- if(sum > summ){
+ if(sum > summ) {
     var demo = document.querySelector('.demo');
-    demo.textContent = `Oq yo’l, Alisher!`
+    demo.textContent = `Oq yo’l, Alisher! '+ sum - summ +'`
 } else {
     var demo = document.querySelector('.demo');
     demo.textContent ='Alisher, ozgina sabr qilish kerak bo’lar ekan'
