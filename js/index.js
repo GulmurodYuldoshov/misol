b3fc97bf48917c21051860b7cdcd97db2fc2affc
Gulmurod Yuldoshov <gystiu1995@gmail.com>
2022-04-29 01:13:06 +0500

    finish

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var a = (500+250)*11191
+var b = 120*11747.42
+var summ = a+b
+
+alert ("Xush kelibsiz");
+ var sum=prompt("summani kirgizing", );
+
+ if(sum > summ){
+    var demo = document.querySelector('.demo');
+    demo.textContent = `Oq yo’l, Alisher!`
+} else {
+    var demo = document.querySelector('.demo');
+    demo.textContent ='Alisher, ozgina sabr qilish kerak bo’lar ekan'
+}
+
+
+
+
+
+
