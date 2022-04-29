43529eb62b44392f05d8cd695a0969068d007a13
Gulmurod Yuldoshov <gystiu1995@gmail.com>
2022-04-30 01:33:16 +0500

    end of exercise

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
-var a = (500+250)*11191
-var b = 120*11747.42
-var summ = a+b
+var a = (500 + 250) * 11191
+var b = 120 * 11747.42
+var summ = a + b
+var bool
 
+alert ("Xush kelibsiz")
 
+ var sum = prompt("summani kirgizing")
+ console.log(typeof sum)
+if(isNaN(sum)){
+    alert("Faqat son kiritishingiz mumkin!")
+    sum = prompt("summani kirgizing")
 
-alert ("Xush kelibsiz");
- var sum=prompt("summani kirgizing", );
-
- if(sum >= summ) {
-    var demo = document.querySelector('.demo');
-    demo.textContent = ` Oq yo’l, Alisher! `
-} else {
-    var demo = document.querySelector('.demo');
-    demo.textContent ='Alisher, ozgina sabr qilish\n kerak bo’lar ekan'
+} else{
+    if(sum >= summ) {
+        var demo = document.querySelector('.demo')
+        demo.textContent = ` Oq yo’l, Alisher! `
+    } else {
+        var demo = document.querySelector('.demo')
+        demo.textContent = 'Alisher, ozgina sabr qilish\n kerak bo’lar ekan'
+    }
 }
 var qoldiq = sum - summ
-var pul = document.querySelector('.naqt');
-pul.textContent =qoldiq
 
+    if(qoldiq >= 0) {
+        var demo = document.querySelector('.demo')
+        demo.textContent = ` Oq yo’l, Alisher! `
+        var pul = document.querySelector('.naqt')
+        pul.textContent = qoldiq
+
+    } else {
+        var demo = document.querySelector('.demo')
+        demo.textContent = ' Alisher, ozgina sabr qilish\n kerak bo’lar ekan'
+        var pul = document.querySelector('.naqt')
+        pul.textContent = " Yo`q"
+    }
 
 
 
