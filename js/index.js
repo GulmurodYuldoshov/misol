d625effa5b74bdf24a8431e5a77c590037f92f22
Gulmurod Yuldoshov <gystiu1995@gmail.com>
2022-04-30 01:43:31 +0500

    end of exercise

--- js/index.js
+++ js/index.js
@@ -1,16 +1,13 @@
 var a = (500 + 250) * 11191
 var b = 120 * 11747.42
 var summ = a + b
-var bool
 
 alert ("Xush kelibsiz")
+var sum = prompt("summani kirgizing")
 
- var sum = prompt("summani kirgizing")
- console.log(typeof sum)
 if(isNaN(sum)){
     alert("Faqat son kiritishingiz mumkin!")
     sum = prompt("summani kirgizing")
-
 } else{
     if(sum >= summ) {
         var demo = document.querySelector('.demo')
@@ -21,13 +18,11 @@ if(isNaN(sum)){
     }
 }
 var qoldiq = sum - summ
-
     if(qoldiq >= 0) {
         var demo = document.querySelector('.demo')
         demo.textContent = ` Oq yo’l, Alisher! `
         var pul = document.querySelector('.naqt')
         pul.textContent = qoldiq
-
     } else {
         var demo = document.querySelector('.demo')
         demo.textContent = ' Alisher, ozgina sabr qilish\n kerak bo’lar ekan'
